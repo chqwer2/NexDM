b5857d70d06b75ad328be501cf3efe83d3f8c7b0
Calvin Chen <calvin.chen@asml.com>
2023-02-13 11:54:29 +0800

    init

---
+++ scripts/data.sh
@@ -0,0 +1,10 @@
+
+
+
+# Download NYU-v2 subsets
+wget -p datasets/NYU/nyu_depth_v2/nyu_depth_v2_labeled.mat \
+      http://horatio.cs.nyu.edu/mit/silberman/nyu_depth_v2/nyu_depth_v2_labeled.mat
+
+# ref to https://cs.nyu.edu/~silberman/datasets/nyu_depth_v2.html#raw_parts
+# NYU-v1
+# http://horatio.cs.nyu.edu/mit/silberman/nyu_depth_data_raw.tar.bz2
---
+++ scripts/env.sh
@@ -0,0 +1,7 @@
+
+source ~/.bashrc
+conda create -n "nex" python=3.8
+
+pip install -r stablediffusion/enviornment.yaml
+pip install -r nex-code/enviornment.yaml
+
